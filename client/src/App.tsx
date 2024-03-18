77d72bd725fe9647d9eace5862ba8a9139ad8b00
stayflyndev <aretee64@gmail.com>
2024-03-18 17:05:12 -0400

    adding styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "react-bootstrap/Button";
